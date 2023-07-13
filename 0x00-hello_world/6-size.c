7fad2da5e35085dc161f154f3df99801a75d72a0
Icraku <kutswaivy@gmail.com>
2023-07-13 07:33:47 -0700

    lastest

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a C program that prints the size of various types on the computer it is compiled and run on
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+return (0);
+}
