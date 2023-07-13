d3db0e4f84d74122b19c20134531f4178be165ed
Icraku <kutswaivy@gmail.com>
2023-07-13 07:58:42 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
+ * main - Entry point
  *
- * * main - Entry point
- *
- * *
- *
- * * Return: Always 1 (Success)
- *
- * */
-
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
 write(2,
