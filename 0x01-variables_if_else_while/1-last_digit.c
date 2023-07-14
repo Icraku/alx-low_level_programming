10286dc5b20420ceaa271f41bcdb86333cf24b1b
Icraku <kutswaivy@gmail.com>
2023-07-14 10:47:29 -0700

    task2

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+/*
+ * File_1:1_last_task.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - C program that prints the last digit of a randomly genwrated number and whether it is greater than 5, less than 6 or 0.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if ((n % 10) > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n",n, n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",n, n % 10);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n",n, n % 10);
+	}
+	return (0);
+}
