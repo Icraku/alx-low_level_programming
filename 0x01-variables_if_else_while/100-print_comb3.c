8a46bc95f3e3852a7e97025e0fc3f290b41e4714
Icraku <kutswaivy@gmail.com>
2023-07-14 12:39:44 -0700

    final advanced task1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		putchar(num + '0');
+		putchar('0' + num);
 		if (num != 9)
 		{
 			putchar(',');
@@ -19,7 +19,7 @@ int main(void)
 		}
 	}
 
-        putchar('\n');
+	putchar('\n');
 
 	return (0);
 }
