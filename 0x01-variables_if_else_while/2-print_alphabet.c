8ea3f6853e3b651cd905bdefb30c42ef121e0635
Icraku <kutswaivy@gmail.com>
2023-07-14 13:53:24 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,4 @@
-/**
+/*
  * File: 2-print_alphabet.c
  * Auth: Brennan D Baraban
  */
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 /**
- * main - Program that prints the alphabet letters in lowercase.
+ * main - Prints the alphabet in lowercase
  *
  * Return: Always 0 (Success)
  */
