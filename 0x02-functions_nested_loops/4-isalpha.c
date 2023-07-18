1da829bbc8be134e143deaceb36c323534220ce0
Icraku <kutswaivy@gmail.com>
2023-07-18 06:24:38 -0700

    final

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,9 +8,9 @@
 
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
-	{
-		return (1);
-	}
-	return (0);
+        if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+        {
+                return (1);
+        }
+        return (0);
 }
