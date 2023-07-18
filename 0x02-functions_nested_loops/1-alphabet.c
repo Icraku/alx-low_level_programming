06276274d99160d5e1806494d38855ec65ccdae8
Icraku <kutswaivy@gmail.com>
2023-07-18 04:52:05 -0700

    prints alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * print_lower_alphabet - Make the alphabet
+ *
+ * Return:void
+ */
+
+void print_lower_alphabet(void)
+{
+	char ch;
+	for  (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+		_putchar('\n');
+}
