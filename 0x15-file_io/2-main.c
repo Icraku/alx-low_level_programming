9cb46e9324935e624b69f9f64a9a444e51841e65
Icraku <kutswaivy@gmail.com>
2023-09-04 09:00:45 -0700

    added descriptor to the main file

--- 0x15-file_io/2-main.c
+++ 0x15-file_io/2-main.c
@@ -4,19 +4,20 @@
 
 /**
  * main - check the code
- *
+ * @ac: argument 1
+ * @av: pointer
  * Return: Always 0.
  */
 int main(int ac, char **av)
 {
-    int res;
+	int res;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = append_text_to_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s filename text\n", av[0]);
+		exit(1);
+	}
+	res = append_text_to_file(av[1], av[2]);
+	printf("-> %i)\n", res);
+	return (0);
 }
