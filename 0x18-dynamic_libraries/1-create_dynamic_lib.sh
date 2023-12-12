632d7736a7b98a5161e2bec32df528deacbe3349
Icraku <kutswaivy@gmail.com>
2023-12-12 07:58:18 -0800

    task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/bash
-gcc -c -fPIC *.c 
-gcc -shared -o liball.so *.o 
+#!/bin/bash
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
